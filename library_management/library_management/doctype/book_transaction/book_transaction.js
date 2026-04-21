41df5811d604204b630169a4cd92cd047f499485
purvaphadol <purvaphadol2gmail.com>
2026-04-21 11:59:17 +0530

    feat(transaction): add Book Transaction doctype with submit workflow

---
+++ library_management/library_management/doctype/book_transaction/book_transaction.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Purva and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Book Transaction', {
+	// refresh: function(frm) {
+
+	// }
+});
